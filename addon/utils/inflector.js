9376dca1d60f4643191832a04418d72e1d909da8
Krystan HuffMenne <krystan@tilde.io>
2019-02-14 13:44:46 -0800

    Fix exports with latest babel

--- addon/utils/inflector.js
+++ addon/utils/inflector.js
@@ -1,4 +1,9 @@
 import { capitalize, camelize, dasherize, underscore } from '@ember/string';
 export { singularize, pluralize } from 'ember-inflector';
 
-export { capitalize, camelize, dasherize, underscore };
+let mCapitalize = capitalize;
+let mCamelize = camelize;
+let mDasherize = dasherize;
+let mUnderscore = underscore;
+
+export { mCapitalize as capitalize, mCamelize as camelize, mDasherize as dasherize, mUnderscore as underscore };
